6f54aacb09ec9076fa261540ffc44c8a683d953c
benconsult <bendayo38@gmail.com>
2024-04-03 13:47:29 +0100

    error checking - controller

--- starter/controllers/auth.js
+++ starter/controllers/auth.js
@@ -1,7 +1,16 @@
 
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
+
+const {BadRequestError} = require('../errors')
+
 const register = async (req, res) =>{
+
+     //controller null check
+     const {name, email, passwword} = req.body
+     if(!name || !email || !passwword){
+        throw new BadRequestError('Please provide name, email, password')
+     }
     const user = await User.create({...req.body})
     res.status(StatusCodes.CREATED).json({user})
     res.send('Register user')
