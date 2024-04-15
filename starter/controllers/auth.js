9e2a05a649c98dd4f32eaae2bd2d5c60aa3d694f
benconsult <bendayo38@gmail.com>
2024-04-15 13:08:53 +0100

    generate token

--- starter/controllers/auth.js
+++ starter/controllers/auth.js
@@ -1,14 +1,16 @@
 
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
-const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const {BadRequestError} = require('../errors')
 
 const register = async (req, res) =>{
     
     const user = await User.create({...req.body})
+    const token = jwt.sign({ userID: user._id, name: user.name}, 'JwtSecret',{ expiresIn: '30d'})
    
-    res.status(StatusCodes.CREATED).json({user})
+    res.status(StatusCodes.CREATED).json({ user: {name: user.name }, token})
     
 }
 
