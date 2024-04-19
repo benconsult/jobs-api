699332418e09cfd3b861e9c53de9f68b1358ae05
benconsult <bendayo38@gmail.com>
2024-04-19 07:35:50 +0100

    validation error from postman

--- starter/middleware/error-handler.js
+++ starter/middleware/error-handler.js
@@ -7,16 +7,23 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || 'Something went wrong, try again later'
   }
   // if (err instanceof CustomAPIError) {
-  //   return res.status(err.statusCode).json({ msg: err.message })
+  //    return res.status(err.statusCode).json({ msg: err.message })
   // }
   //set duplicate error message
+  
+  //vlaidation error
+  if(err.name === 'ValidationError'){
+    customError.msg = Object.values(err.errors).map((item)=> item.message).join(',')
+    customError.statusCode = 400
+  }
   if(err.code && err.code === 11000){
-    customError.msg = `Duplicate value entered for ${err.keyValue}, please choose another value`
+    customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)}, please choose another value`
     customError.statusCode = 400
   } 
 
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
   return res.status(customError.statusCode).json({ msg: customError.msg })
-  //return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  
 }
 
 module.exports = errorHandlerMiddleware
